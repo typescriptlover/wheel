dd834822eca6eef522bc9a7487d4e7ab05814ebd
guy <devtyl3r@gmail.com>
2022-11-23 17:17:12 -0400

    feat&fix: env, getconfig, jwt_secret

--- apps/api/config/config.go
+++ apps/api/config/config.go
@@ -4,15 +4,21 @@ import (
 	"log"
 
 	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
 )
 
 type config struct {
-	DB_URI string `env:"DB_URI"`
+	DB_URI     string `env:"DB_URI"`
+	JWT_SECRET string `env:"JWT_SECRET"`
 }
 
 var Config *config
 
 func Init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+
 	c := &config{}
 	opts := env.Options{RequiredIfNoDef: true}
 
@@ -21,4 +27,8 @@ func Init() {
 	}
 
 	Config = c
+}
+
+func GetConfig() *config {
+	return Config
 }
